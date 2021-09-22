2c8d7fbecfacc4c00b683cc21dc284083ef1d125
davbao <davbao.py@gmail.com>
2021-09-22 19:32:07 +0800

    反转字符串

---
+++ src/main/java/algo/easy/Nc103ReverseStr.java
@@ -0,0 +1,33 @@
+package main.java.algo.easy;
+
+/**
+ * @author davbao
+ * @date 2021/9/22
+ */
+public class Nc103ReverseStr {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solve("1234"));
+        System.out.println(new Solution().solve("12345"));
+    }
+}
+
+class Solution {
+    /**
+     * 反转字符串
+     * @param str string字符串
+     * @return string字符串
+     */
+    public String solve (String str) {
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+        char[] arr = str.toCharArray();
+        int end = arr.length % 2 != 0 ? (arr.length - 1) / 2 : arr.length / 2 - 1;
+        for (int i = 0; i <= end; i++) {
+            char temp = arr[i];
+            arr[i] = arr[arr.length-1-i];
+            arr[arr.length-1-i] = temp;
+        }
+        return String.valueOf(arr);
+    }
+}
---
+++ src/main/java/common/ListNode.java
@@ -0,0 +1,14 @@
+package main.java.common;
+
+/**
+ * @author davbao
+ * @date 2021/9/22
+ */
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val= val;
+    }
+}
